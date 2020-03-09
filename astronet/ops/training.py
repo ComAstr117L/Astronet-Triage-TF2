735599a376a01ede0887603e693b29a7f047c16d
Abhishyant Khare <Abhishyant Khare>
2020-03-08 19:38:04 -0700

    Add functionality from tf_slim to replace tf.contrib functionality
    TLDR: Google sucks, this was harder than it needed to be, but the TF2
    issues should be solved now.
    
    The issue here was that usage of tf.contrib cannot be replaced
    automatically by the upgrade script. It seems that several libraries in
    tf.contrib have not been automatically ported. Google really bungled the
    upgrade process from TF1 to TF2. They have not managed backwards
    compatability well with little to no thought to existing users. This
    was not an easy task to solve, and the only entity to blame here is
    Google. You can track the upgrade process of TF1 contrib functionality
    here:
    https://docs.google.com/spreadsheets/d/1FLFJLzg7WNP6JHODX5q8BDgptKafq_slHpnHVbJIteQ/edit#gid=0
    (Notice how empty the sheet is)
    
    Fortunately, the functionality required by astronet was ported to a
    separate library Google created called tf-slim. There is no
    documentation that they did that, and it is instead left to the user to
    discover that. Why not include that information in the TF2 upgrade page?
    Who knows why Google seems to hate their users?
    
    However, it's not just as easy pip installing tf-slim, because while
    the GitHub repository has updated code, the actual pip package is not
    updated and still contains references to tf.contrib. Why not update the
    pip package if its such a major release? No good reason other than pure
    mismanagement of this upgrade process. I don't know if anyone at Google
    really even thought through this well.
    
    Anyways, I managed to cherry pick the exact functionality astronet
    needed from tf-slim and put it into astronet itself and modified the
    imports. Everything should work correctly now.

--- astronet/ops/training.py
+++ astronet/ops/training.py
@@ -19,88 +19,89 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+import astronet.ops.slim_training as slim_training
 
 
 def create_learning_rate(hparams, global_step):
-  """Creates a learning rate Tensor.
+    """Creates a learning rate Tensor.
 
-  Args:
-    hparams: ConfigDict containing the learning rate configuration.
-    global_step: The global step Tensor.
+    Args:
+      hparams: ConfigDict containing the learning rate configuration.
+      global_step: The global step Tensor.
 
-  Returns:
-    A learning rate Tensor.
-  """
-  if hparams.get("learning_rate_decay_factor"):
-    learning_rate = tf.compat.v1.train.exponential_decay(
-        learning_rate=float(hparams.learning_rate),
-        global_step=global_step,
-        decay_steps=hparams.learning_rate_decay_steps,
-        decay_rate=hparams.learning_rate_decay_factor,
-        staircase=hparams.learning_rate_decay_staircase)
-  else:
-    learning_rate = tf.constant(hparams.learning_rate)
+    Returns:
+      A learning rate Tensor.
+    """
+    if hparams.get("learning_rate_decay_factor"):
+        learning_rate = tf.compat.v1.train.exponential_decay(
+            learning_rate=float(hparams.learning_rate),
+            global_step=global_step,
+            decay_steps=hparams.learning_rate_decay_steps,
+            decay_rate=hparams.learning_rate_decay_factor,
+            staircase=hparams.learning_rate_decay_staircase)
+    else:
+        learning_rate = tf.constant(hparams.learning_rate)
 
-  return learning_rate
+    return learning_rate
 
 
 def create_optimizer(hparams, learning_rate, use_tpu=False):
-  """Creates a TensorFlow Optimizer.
+    """Creates a TensorFlow Optimizer.
 
-  Args:
-    hparams: ConfigDict containing the optimizer configuration.
-    learning_rate: A Python float or a scalar Tensor.
-    use_tpu: If True, the returned optimizer is wrapped in a
-        CrossShardOptimizer.
+    Args:
+      hparams: ConfigDict containing the optimizer configuration.
+      learning_rate: A Python float or a scalar Tensor.
+      use_tpu: If True, the returned optimizer is wrapped in a
+          CrossShardOptimizer.
 
-  Returns:
-    A TensorFlow optimizer.
+    Returns:
+      A TensorFlow optimizer.
 
-  Raises:
-    ValueError: If hparams.optimizer is unrecognized.
-  """
-  optimizer_name = hparams.optimizer.lower()
-  if optimizer_name == "momentum":
-    optimizer = tf.compat.v1.train.MomentumOptimizer(
-        learning_rate,
-        momentum=hparams.get("momentum", 0.9),
-        use_nesterov=hparams.get("use_nesterov", False))
-  elif optimizer_name == "sgd":
-    optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate)
-  elif optimizer_name == "adagrad":
-    optimizer = tf.compat.v1.train.AdagradOptimizer(learning_rate)
-  elif optimizer_name == "adam":
-    optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate)
-  elif optimizer_name == "rmsprop":
-    optimizer = tf.RMSPropOptimizer(learning_rate)
-  else:
-    raise ValueError("Unknown optimizer: %s" % hparams.optimizer)
+    Raises:
+      ValueError: If hparams.optimizer is unrecognized.
+    """
+    optimizer_name = hparams.optimizer.lower()
+    if optimizer_name == "momentum":
+        optimizer = tf.compat.v1.train.MomentumOptimizer(
+            learning_rate,
+            momentum=hparams.get("momentum", 0.9),
+            use_nesterov=hparams.get("use_nesterov", False))
+    elif optimizer_name == "sgd":
+        optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate)
+    elif optimizer_name == "adagrad":
+        optimizer = tf.compat.v1.train.AdagradOptimizer(learning_rate)
+    elif optimizer_name == "adam":
+        optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate)
+    elif optimizer_name == "rmsprop":
+        optimizer = tf.RMSPropOptimizer(learning_rate)
+    else:
+        raise ValueError("Unknown optimizer: %s" % hparams.optimizer)
 
-  if use_tpu:
-    optimizer = tf.compat.v1.tpu.CrossShardOptimizer(optimizer)
+    if use_tpu:
+        optimizer = tf.compat.v1.tpu.CrossShardOptimizer(optimizer)
 
-  return optimizer
+    return optimizer
 
 
 def create_train_op(model, optimizer):
-  """Creates a Tensor to train the model.
+    """Creates a Tensor to train the model.
 
-  Args:
-    model: Instance of AstroModel.
-    optimizer: Instance of tf.train.Optimizer.
+    Args:
+      model: Instance of AstroModel.
+      optimizer: Instance of tf.train.Optimizer.
 
-  Returns:
-    A Tensor that runs a single training step and returns model.total_loss.
-  """
-  # Maybe clip gradient norms.
-  transform_grads_fn = None
-  if model.hparams.get("clip_grad_norm"):
-    transform_grads_fn = tf.contrib.training.clip_gradient_norms_fn(
-        model.hparams.clip_gradient_norm)
+    Returns:
+      A Tensor that runs a single training step and returns model.total_loss.
+    """
+    # Maybe clip gradient norms.
+    transform_grads_fn = None
+    if model.hparams.get("clip_grad_norm"):
+        transform_grads_fn = slim_training.clip_gradient_norms_fn(
+            model.hparams.clip_gradient_norm)
 
-  # Create train op.
-  return tf.contrib.training.create_train_op(
-      total_loss=model.total_loss,
-      optimizer=optimizer,
-      global_step=model.global_step,
-      transform_grads_fn=transform_grads_fn)
+    # Create train op.
+    return slim_training.create_train_op(
+        total_loss=model.total_loss,
+        optimizer=optimizer,
+        global_step=model.global_step,
+        transform_grads_fn=transform_grads_fn)
